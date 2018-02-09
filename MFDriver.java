9ebf0988c64633f8f69737bc08afee1e23168dcb
Kenny <lli15@stuy.edu>
2018-02-09 01:43:24 -0500

    Implemented time testing algorithm

--- MFDriver.java
+++ MFDriver.java
@@ -7,6 +7,26 @@ public class MFDriver{
 		i++;
 	    }
 	}
+	}
+	
+	public static void collectData() {
+		for (int n = 1000; n <= 10000; n+= 100) {
+			long avg = 0;
+			for (int x = 0; x < 10; x++) {
+				int[][] temp = new int[n][n];
+				populate(temp);
+				long nano = System.nanoTime();			
+				MatrixFinder.search(temp, temp[n - 1][0]);
+				long diff = System.nanoTime() - nano;
+				avg += diff;
+			}
+			avg /= 10;
+			System.out.println(n + "," + avg);
+		}
     }
-    public static void main(String[]args){}
+	
+    public static void main(String[]args){
+		System.out.println("matrix size(n), time(nanoseconds)");
+		collectData();
+	}
 }
